f42d77a996624405134e7d83c47fa6ca5b3fbb6f
theeraphong0 <yugilive@hotmail.com>
2016-10-31 15:17:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,3 @@
-curl -X GET \
--H 'Authorization: Bearer {s5Scyfs8jIVZvETu40OgTWuSpzZkFImt7w8fZqyKjhbNUAnB2zsFuXLUW0DClcBkss/4+mWdC1EGrJiWuNW1zTMhEPgSQStkceEcnfASjGQqfvFLxpO0baQiIJKl0RswNN3xNZUd9wWQi4J4zQsx9wdB04t89/1O/w1cDnyilFU=}' \
-https://api.line.me/v1/oauth/verify
-
+<?php
+echo "hello";
+?>
