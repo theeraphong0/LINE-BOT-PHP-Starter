72345d096c83e9f78f58ef1a729a066038cb2908
theeraphong0 <yugilive@hotmail.com>
2016-11-28 15:51:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,12 +19,11 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				
-				{'type' => 'text',
-				'text' => "Hello, world"
+				{
+				'type' => 'text',
+				'text' => $message
 				}
-				{"type": "text",
-    				"text": "Hello1, world1"
-				}
+
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
