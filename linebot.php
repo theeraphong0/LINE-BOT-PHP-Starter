14e17c2d84e014178070ac9dff5c08f7aa91f3a7
theeraphong0 <yugilive@hotmail.com>
2017-03-31 21:18:09 +0700

    Update linebot.php

--- linebot.php
+++ linebot.php
@@ -26,7 +26,14 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else{
+}
+else if($arrJson['events'][0]['message']['text'] == "รักมั้ย"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "รักมากๆ";
+}
+else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
