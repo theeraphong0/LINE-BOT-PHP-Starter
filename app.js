2da9fcd2f0c9da97eafd365d0108e0d7fe19cfb6
theeraphong0 <yugilive@hotmail.com>
2017-04-04 10:17:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-var http = require('http');
+var http = require('app');
 var ip = '127.0.0.1';
 var port = 1337;
- 
-http.createServer(function (req, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(port,ip);
- 
-console.log('Server running at http://' + ip + ':' + port);
+console.log(port);
