43a886a9469e33698966ddd86656ad4e36154878
theeraphong0 <yugilive@hotmail.com>
2017-04-05 12:39:39 +0700

    Update phpmqtt.php

--- phpmqtt.php
+++ phpmqtt.php
@@ -1,28 +1,4 @@
   <?php
 
-  require("phpmqtt.php");
-
-  $host = "m13.cloudmqtt.com"; 
-  $port = 14476;
-  $username = "xwspenyh"; 
-  $password = "GoKe-ybHgMXi"; 
-
-  $mqtt = new phpMQTT($host, $port, "ClientID".rand()); 
-
-  if(!$mqtt->connect(true,NULL,$username,$password)){
-    exit(1);
-  }
-
-  //currently subscribed topics
-  $topics['topic'] = array("qos"=>0, "function"=>"procmsg");
-  $mqtt->subscribe($topics,0);
-
-  while($mqtt->proc()){        
-  }
-
-  $mqtt->close();
-  function procmsg($topic,$msg){
-    echo "Msg Recieved: $msg";
-  }
 echo "HELLO World";
 ?> 
