7e9b0f7e4dde078e0d6c3024c382128a4cc99cff
theeraphong0 <yugilive@hotmail.com>
2017-04-18 17:48:44 +0700

    Update phpmqtt.php

--- phpmqtt.php
+++ phpmqtt.php
@@ -1,4 +1,15 @@
-  <?php
+  $host = "m13.cloudmqtt.com"; 
+  $port = 14476;
+  $username = "xwspenyh"; 
+  $password = "GoKe-ybHgMXi"; 
+  $message = "Hello CloudMQTT!";
 
-echo "HELLO World";
-?> 
+  //MQTT client id to use for the device. "" will generate a client id automatically
+  $mqtt = new phpMQTT($host, $port, "ClientID".rand()); 
+
+  if ($mqtt->connect(true,NULL,$username,$password)) {
+    $mqtt->publish("topic",$message, 0);
+    $mqtt->close();
+  }else{
+    echo "Fail or time out<br />";
+  }
