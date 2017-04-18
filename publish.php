f51919ba8cafc95d0e2681f17935fb7b83f9d6f9
theeraphong0 <yugilive@hotmail.com>
2017-04-18 18:06:09 +0700

    Update publish.php

--- publish.php
+++ publish.php
@@ -1,9 +1,18 @@
-<?php
-require("phpMQTT.php");
-	
-$mqtt = new phpMQTT("m13.cloudmqtt.com", 14476, "phpMQTT Pub Example"); //Change client name to something unique
-if ($mqtt->connect()) {
-	$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest","Hello World! at ".date("r"),0);
-	$mqtt->close();
-}
-?>
+  //publish.php
+  require("phpMQTT.php");
+
+  $host = "m13.cloudmqtt.com"; 
+  $port = port;
+  $username = "xwspenyh"; 
+  $password = "GoKe-ybHgMXi"; 
+  $message = "Hello CloudMQTT!";
+
+  //MQTT client id to use for the device. "" will generate a client id automatically
+  $mqtt = new phpMQTT($host, $port, "ClientID".rand()); 
+
+  if ($mqtt->connect(true,NULL,$username,$password)) {
+    $mqtt->publish("topic",$message, 0);
+    $mqtt->close();
+  }else{
+    echo "Fail or time out<br />";
+  }
